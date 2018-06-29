1ac124fa0baa83d26efd0e4d6865f94749ced07f
Joshua Levine <joshuapark95@gmail.com>
2018-06-29 13:17:08 -0500

    Enforce ownership of hot wallet via 'approve link' functionality

--- src/VoteProxyFactory.sol
+++ src/VoteProxyFactory.sol
@@ -8,25 +8,35 @@ contract VoteProxyFactory {
     DSToken public iou;
     mapping(address=>VoteProxy) public hotMap;
     mapping(address=>VoteProxy) public coldMap;
+    mapping(address=>address) public desiredLink;
     
     constructor(DSChief chief_) public {
         chief = chief_;
         gov = chief.GOV();
         iou = chief.IOU();
     }
-    
-    function newVoteProxy(address cold_, address hot_) public returns (VoteProxy voteProxy) {
-        require(cold_ == msg.sender, "Vote Proxy must be created by cold wallet");
-        require(
-            coldMap[cold_] == address(0) && hotMap[cold_] == address(0), 
-            "Cold wallet cannot already have a Vote Proxy associated with it"
+
+    function approveLink(address cold_) public returns (VoteProxy voteProxy) {
+        address hot_ = msg.sender;
+        requre(
+            desiredLink[cold_] == hot_, 
+            "Cold wallet must have initiated a link"
             );
-       require(
+        require(
             coldMap[hot_] == address(0) && hotMap[hot_] == address(0), 
             "Hot wallet cannot already have a Vote Proxy associated with it"
             );
         voteProxy = new VoteProxy(gov, chief, iou, cold_, hot_);
         hotMap[hot_] = voteProxy;
         coldMap[cold_] = voteProxy;
+    }
+    
+    function initiateLink(address hot_) public {
+        address cold_ = msg.sender;
+        require(
+            coldMap[cold_] == address(0) && hotMap[cold_] == address(0), 
+            "Cold wallet cannot already have a Vote Proxy associated with it"
+            );
+        desiredLink[cold_] = hot_;
     }
 }

e66b0ed8b81eec1459997ce9781ecde92bb7565b
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-05-04 13:51:21 -0300

    Add license headers

--- src/VoteProxyFactory.sol
+++ src/VoteProxyFactory.sol
@@ -1,4 +1,21 @@
-// VoteProxyFactory - create and keep record of proxy identities
+/// VoteProxyFactory.sol
+
+// Copyright (C) 2018-2020 Maker Ecosystem Growth Holdings, INC.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// create and keep record of proxy identities
 pragma solidity >=0.4.24;
 
 import "./VoteProxy.sol";
